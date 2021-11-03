c1cb8d130db8645262823110a4fd0d7885f7bd85
fsd72021 <82801904+fsd72021@users.noreply.github.com>
2021-11-03 16:59:08 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,210 @@
+$(document).ready(function () {
+  "use strict";
+
+  //show nav link underline 
+  $(".nav-link").on("mouseleave", function () {
+    $(this).addClass("change")
+  });
+  $(".nav-link").on("mousemove", function () {
+    $(this).removeClass("change")
+  });
+
+  // fixed header
+  $(window).scroll(function () {
+    let scroll = $(window).scrollTop();
+
+    if (scroll > 1) {
+      $("header").addClass("fixed");
+    } else {
+      $("header").removeClass("fixed");
+    }
+  });
+
+  //main slider owl
+  $(".main-slider-carousel").owlCarousel({
+    loop: true,
+    autoplay: true,
+    autoplayTimeout: 10000,
+    items: 1,
+    nav: false,
+    dots: true,
+    rtl: true,
+    smartSpeed: 1000,
+    // animateOut: "slideOutUp",
+    // animateIn: "slideInDown",
+    navText: ["<i class='bi bi-arrow-left'></i>", "<i class='bi bi-arrow-right'></i>"],
+    responsive: {
+      0: {
+        items: 1,
+        nav: false,
+        dots: true,
+      },
+      768: {
+        items: 1,
+        nav: true,
+      },
+
+      992: {
+        items: 1,
+      }
+    }
+  });
+
+  //why slider owl
+  $(".why-carousel").owlCarousel({
+    loop: true,
+    autoplay: true,
+    autoplayTimeout: 5000,
+    items: 4,
+    nav: true,
+    dots: false,
+    rtl: true,
+    smartSpeed: 2000,
+    margin: 20,
+    navText: ["<i class='bi bi-arrow-left'></i>", "<i class='bi bi-arrow-right'></i>"],
+    responsive: {
+      0: {
+        items: 2,
+        nav: false,
+        dots: true,
+      },
+      768: {
+        items: 3,
+        nav: true,
+      },
+
+      992: {
+        items: 4,
+      }
+    }
+  });
+
+  //clients slider owl
+  $(".clients-carousel").owlCarousel({
+    loop: true,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    items: 5,
+    nav: false,
+    dots: false,
+    rtl: true,
+    margin: 50,
+    navText: ["<i class='bi bi-arrow-left'></i>", "<i class='bi bi-arrow-right'></i>"],
+    responsive: {
+      0: {
+        items: 2,
+      },
+      768: {
+        items: 3,
+      },
+
+      992: {
+        items: 5,
+      }
+    }
+  });
+
+  //blogs slider owl
+  $(".blogs-carousel").owlCarousel({
+    loop: true,
+    autoplay: true,
+    autoplayTimeout: 5000,
+    items: 3,
+    nav: false,
+    dots: true,
+    rtl: true,
+    smartSpeed: 2000,
+    margin: 20,
+    navText: ["<i class='bi bi-arrow-left'></i>", "<i class='bi bi-arrow-right'></i>"],
+    responsive: {
+      0: {
+        items: 2,
+        nav: false,
+        dots: true,
+      },
+      768: {
+        items: 3,
+        nav: true,
+      },
+
+      992: {
+        items: 3,
+      }
+    }
+  });
+
+  //to top button
+  $(window).scroll(function () {
+    if ($(this).scrollTop() >= 500) {
+      $("#toTop").fadeIn(100);
+    } else {
+      $("#toTop").fadeOut(100);
+    }
+  });
+
+  $("#toTop").click(function () {
+    $("html, body").animate({
+      scrollTop: 0
+    }, 0);
+  });
+
+  //validate form
+  (function () {
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    let forms = document.querySelectorAll(".needs-validation")
+
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+      .forEach(function (form) {
+        form.addEventListener("submit", function (event) {
+          if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+          } else {
+            event.preventDefault()
+          }
+          form.classList.add("was-validated")
+        }, false)
+      })
+  })();
+
+  //store theme colors in local storage 
+  if (!localStorage.getItem("main_color")) {
+    $(":root").css("--main-color", "#87D3F2");
+  } else {
+    $(":root").css("--main-color", localStorage.getItem("main_color"));
+  }
+
+  if (!localStorage.getItem("second_color")) {
+    $(":root").css("--second-color", "#155CB4");
+  } else {
+    $(":root").css("--second-color", localStorage.getItem("second_color"));
+  }
+
+  if (!localStorage.getItem("dark_color")) {
+    $(":root").css("--dark-color", "#2E2E38");
+  } else {
+    $(":root").css("--dark-color", localStorage.getItem("dark_color"));
+  }
+
+  //lazyload images
+  $("img").Lazy({
+    scrollDirection: "vertical",
+    effect: "fadeIn",
+    visibleOnly: false,
+  });
+});
+
+$(window).on("load", function () {
+  $('.loader').delay(500).fadeOut(500);
+
+  //wow animate
+  wow = new WOW({
+    boxClass: 'wow', // default
+    animateClass: 'animated', // default
+    offset: 0, // default
+    mobile: false, // default
+    live: true // default
+  })
+  // wow.init();
+});
